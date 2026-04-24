c33ea843afdcadf954563799398d5cb1b4a0bd27
Christine Traver <Christine Traver@northeastdata.local>
2026-04-24 16:58:42 -0400

    add paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -220,7 +220,7 @@ export default function App() {
             <p className="mt-1 text-white/80">Monday - Wednesday 5:30 PM to 9:00 PM</p>
             <p className="text-white/80">Thursday - Friday: By Appointment (5PM-9PM)</p>
             <p className="text-white/80">Saturday - Sunday: By Appointment (8AM-7PM)</p>
-            <p></p> 
+            <p className="text-white/80"> </p>
             <p className="text-white/80">To schedule an appointment call or text (570) 396-4808</p>
           </div>
 
